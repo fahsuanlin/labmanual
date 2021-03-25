53be9112acf778d94d250e28af3941dd339e475e
Fa-Hsuan Lin <fahsuanlin@gmail.com>
2021-03-24 21:35:24 -0400

    Add files via upload

--- scripts/get_mc_regressor.m
+++ scripts/get_mc_regressor.m
@@ -2,11 +2,16 @@ close all; clear all;
 
 fmc_stem='fmcpr.mcdat';
 
-bold_dir='/space/maki7/1/fhlin/seeg/s026/fmri_data/unpack';
+bold_dir='/space_lin2/fhlin/7t_music_skku/LAM_AUD_BHC_simple/unpack';
 
 dirs={
-'bold/030';
-'bold/032';
+'bold/007';
+'bold/008';
+'bold/009';
+'bold/018';
+'bold/019';
+'bold/020';
+'bold/021';
 };
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -22,7 +27,9 @@ for d_idx=1:length(dirs)
 	mc_regressor{d_idx}=mc(:,2:7);
 
 end;
-save mc_regressor.mat mc_regressor
+mc_dirs=dirs;
+
+save mc_regressor.mat mc_regressor mc_dirs
 
 fprintf('DONE!\n');
 
--- scripts/get_ventricle_wm_regressor.m
+++ scripts/get_ventricle_wm_regressor.m
@@ -1,24 +1,57 @@
 close all; clear all;
 
-target_subject='fsaverage';
+target_subject='LAM_AUD_BHC_simple';
 file_register_source={
-    '/space/maki7/1/fhlin/seeg/s026/fmri_data/unpack/bold/030/fmcprstc.nii.gz';
-    '/space/maki7/1/fhlin/seeg/s026/fmri_data/unpack/bold/032/fmcprstc.nii.gz';
-    };
-%'mb_run_5_ref.mgh';
-file_regression_source={
-    '/space/maki7/1/fhlin/seeg/s026/fmri_data/unpack/bold/030/fmcprstc.nii.gz';
-    '/space/maki7/1/fhlin/seeg/s026/fmri_data/unpack/bold/032/fmcprstc.nii.gz';
+    '../unpack/bold/007/fmcprstc.nii.gz';
+    '../unpack/bold/008/fmcprstc.nii.gz';
+    '../unpack/bold/009/fmcprstc.nii.gz';
+    '../unpack/bold/018/fmcprstc.nii.gz';
+    '../unpack/bold/019/fmcprstc.nii.gz';
+    '../unpack/bold/020/fmcprstc.nii.gz';
+    '../unpack/bold/021/fmcprstc.nii.gz';                                                                                                                  
     };
 
-%'mb_run_5_acc.mat';
-file_register='./native2fsaverage.dat';
-file_aseg='aparc+aseg_f.nii';
+file_regression_source={
+    '../unpack/bold/007/fmcprstc.nii.gz';
+    '../unpack/bold/008/fmcprstc.nii.gz';
+    '../unpack/bold/009/fmcprstc.nii.gz';
+    '../unpack/bold/018/fmcprstc.nii.gz';
+    '../unpack/bold/019/fmcprstc.nii.gz';
+    '../unpack/bold/020/fmcprstc.nii.gz';
+    '../unpack/bold/021/fmcprstc.nii.gz';                                                                                                                  
+    };
+
+file_register={
+    './bb_register_007.dat';
+    './bb_register_008.dat';
+    './bb_register_009.dat';
+    './bb_register_018.dat';
+    './bb_register_019.dat';
+    './bb_register_020.dat';
+    './bb_register_021.dat';                                                                                                                               
+    };
+
+file_aseg={
+	'aparc+aseg_fmcprstc_007.nii';
+        'aparc+aseg_fmcprstc_008.nii';
+        'aparc+aseg_fmcprstc_009.nii';
+        'aparc+aseg_fmcprstc_018.nii';
+        'aparc+aseg_fmcprstc_019.nii';
+        'aparc+aseg_fmcprstc_020.nii';
+        'aparc+aseg_fmcprstc_021.nii';                                                                                                                     
+};
+
 file_output={
-    'regressor_wm_ventrical_030.mat';
-    'regressor_wm_ventrical_032.mat';
+    'regressor_wm_ventrical_007.mat';
+    'regressor_wm_ventrical_008.mat';
+    'regressor_wm_ventrical_009.mat';
+    'regressor_wm_ventrical_018.mat';
+    'regressor_wm_ventrical_019.mat';
+    'regressor_wm_ventrical_020.mat';
+    'regressor_wm_ventrical_021.mat';                                                                                                                      
     };
 %'regressor_wm_ventricle_123016.mat';
+
 
 for f_idx=1:length(file_regression_source)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -27,14 +60,22 @@ for f_idx=1:length(file_regression_source)
     %"fsaverage". This registration matrix will be name as
     %"native2fsaverage.dat".
     %eval('!fslregister --s fsaverage --mov bold/004/f.nii --reg ./native2fsaverage.dat --initxfm --maxangle 70');
-    eval(sprintf('!fslregister --s %s --mov %s --reg %s --initxfm --maxangle 70',target_subject, file_register_source{f_idx}, file_register));
+    if(isempty(file_register{f_idx}))
+        [dummy,fstem]=file_output{f_idx};
+        file_register{f_idx}=sprintf('%s_register.dat',fstem);
+        fprintf('creating registration...[%s]....\r',file_register{f_idx});
+        eval(sprintf('!fslregister --s %s --mov %s --reg %s --initxfm --maxangle 70',target_subject, file_register_source{f_idx}, file_register{f_idx}));
+    else
+        fprintf('using registration...[%s]....\r',file_register{f_idx});
+    end;
     
     %apply the "inverse" of the registration such that the aparc+aseg.mgz from
     %"fsaverage" will be transformed to the native subject's anatomical space.
     %The transformed apart+aseg file will be named as "aparc+aseg_f.nii".
     %eval('!mri_vol2vol --mov bold/004/fmc.nii --targ $SUBJECTS_DIR/fsaverage/mri/aparc+aseg.mgz --reg native2fsaverage.dat --o aparc+aseg_f.nii --inv --interp nearest');
-    eval(sprintf('!mri_vol2vol --mov %s --targ $SUBJECTS_DIR/%s/mri/aparc+aseg.mgz --reg %s --o %s --inv --interp nearest',file_regression_source{f_idx},  target_subject, file_register, file_aseg));
-    
+    eval(sprintf('!mri_vol2vol --mov %s --targ $SUBJECTS_DIR/%s/mri/aparc+aseg.mgz --reg %s --o %s --inv --interp nearest',file_regression_source{f_idx},  target_subject, file_register{f_idx}, file_aseg{f_idx}));
+   
+ 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %
     %get white-matter and ventrical from "FreeSurferColorLUT.txt"
@@ -53,7 +94,7 @@ for f_idx=1:length(file_regression_source)
     % 76  Right-Lateral-Ventricles                120 18  134 0
     
     
-    d_aseg=MRIread(file_aseg);
+    d_aseg=MRIread(file_aseg{f_idx});
     v_aseg=d_aseg.vol;
     
     %d_regression=MRIread(file_regression_source);
