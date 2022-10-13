a9c2cb0f735d6e06e2d84337df4400c98c75d7e0
Fa-Hsuan Lin <fahsuanlin@gmail.com>
2022-10-13 01:30:27 -0400

    Add files via upload

--- scripts/show_electrode_preop_mri.m
+++ scripts/show_electrode_preop_mri.m
@@ -1,31 +1,31 @@
 close all; clear all;
 
-setenv('SUBJECTS_DIR','/Users/fhlin_admin/workspace/seeg/subjects/'); %for MAC/Linux
-%setenv('SUBJECTS_DIR','D:\fhlin\Users\fhlin_admin\workspace\seeg\subjects'); %for PC
+setenv('SUBJECTS_DIR','/Users/fhlin/workspace/music_layer_seeg/subjects/'); %for MAC/Linux
+%setenv('SUBJECTS_DIR','D:\fhlin\Users\fhlin\workspace\seeg\subjects'); %for PC
 
-mri_post=MRIread('/Users/fhlin_admin/workspace/seeg/subjects/s031_post/mri/orig.mgz'); %for MAC/Linux
-%mri_post=etc_MRIread('D:\fhlin\Users\fhlin_admin\workspace\seeg\subjects\2036_post\mri\orig.mgz'); %for PC
+mri_post=MRIread('/Users/fhlin/workspace/music_layer_seeg/subjects/s035_post/mri/orig.mgz'); %for MAC/Linux
+%mri_post=etc_MRIread('D:\fhlin\Users\fhlin\workspace\seeg\subjects\2036_post\mri\orig.mgz'); %for PC
 
-mri=MRIread('/Users/fhlin_admin/workspace/seeg/subjects/s031/mri/orig.mgz'); %for MAC/Linux
-%mri=etc_MRIread('D:\fhlin\Users\fhlin_admin\workspace\seeg\subjects\2036\mri\orig.mgz'); %for PC
+mri=MRIread('/Users/fhlin/workspace/music_layer_seeg/subjects/s035/mri/orig.mgz'); %for MAC/Linux
+%mri=etc_MRIread('D:\fhlin\Users\fhlin\workspace\seeg\subjects\2036\mri\orig.mgz'); %for PC
 
 % do registration between pre- and post-OP by the following command:
 %
-% cd /Users/fhlin_admin/workspace/seeg/subjects/s031_post/tmp
-% bbregister --s s031 --mov ../mri/orig.mgz --init-fsl --reg register.dat --t1
+% cd /Users/fhlin/workspace/music_layer_seeg/subjects/s035_post/tmp
+% bbregister --s s035 --mov ../mri/orig.mgz --init-fsl --reg register.dat --t1
 %
-xfm=etc_read_xfm('file_xfm','/Users/fhlin_admin/workspace/seeg/subjects/s031_post/tmp/register.dat'); %for MAC/Linux
-%xfm=etc_read_xfm('file_xfm','D:\fhlin\Users\fhlin_admin\workspace\seeg\subjects\2036_post\tmp\register.dat'); %for PC
+xfm=etc_read_xfm('file_xfm','/Users/fhlin/workspace/music_layer_seeg/subjects/s035_post/tmp/register.dat'); %for MAC/Linux
+%xfm=etc_read_xfm('file_xfm','D:\fhlin\Users\fhlin\workspace\seeg\subjects\2036_post\tmp\register.dat'); %for PC
 
 %load the Talairach transformation matrix from the "pre-OP" data
-talxfm=etc_read_xfm('file_xfm','/Users/fhlin_admin/workspace/seeg/subjects/s031/mri/transforms/talairach.xfm'); %for MAC/Linux
-%talxfm=etc_read_xfm('file_xfm','D:\fhlin\Users\fhlin_admin\workspace\seeg\subjects\2036\mri\transforms\talairach.xfm'); %for PC
+talxfm=etc_read_xfm('file_xfm','/Users/fhlin/workspace/music_layer_seeg/subjects/s035/mri/transforms/talairach.xfm'); %for MAC/Linux
+%talxfm=etc_read_xfm('file_xfm','D:\fhlin\Users\fhlin\workspace\seeg\subjects\2036\mri\transforms\talairach.xfm'); %for PC
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % convert electrode coordinates from post-op MRI to pre-op MRI
 
-file_mat='electrode_040119_085342.mat'; %electrode coordinates in post-op MRI
+file_mat='electrode_041819_114159.mat'; %electrode coordinates in post-op MRI
 load(file_mat);
 
 electrode_out=electrode;
@@ -46,10 +46,10 @@ electrode=electrode_out;
 
 [dummy,fstem]=fileparts(file_mat);
 
-fprintf('\nmust load [%s] to locate electrodes in ***pre-op*** MRI!\n\n',sprintf('%s_s031.mat',fstem));
-save(sprintf('%s_s031.mat',fstem),'electrode');
+fprintf('\nmust load [%s] to locate electrodes in ***pre-op*** MRI!\n\n',sprintf('%s_s035.mat',fstem));
+save(sprintf('%s_s035.mat',fstem),'electrode');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-etc_render_fsbrain('surf','orig','hemi','rh','subject','s031','vol',mri,'talxfm',(talxfm));
+etc_render_fsbrain('surf','orig','hemi','rh','subject','s035','vol',mri,'talxfm',(talxfm));
 view(90,30);
