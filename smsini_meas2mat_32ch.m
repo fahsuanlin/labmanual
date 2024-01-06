3eaf29908de4d048b34001e24cf046b7737384e6
Fa-Hsuan Lin <fahsuanlin@gmail.com>
2024-01-06 18:37:54 -0500

    010624

--- smsini_meas2mat_32ch.m
+++
@@ -1,8 +0,0 @@
-close all; clear all;
-
-addpath(genpath('/autofs/space/maki6_001/users/eva/toolbox/tool_mb_recon/'));
-
-pdir=pwd;
-subfolder_for_meas='32ch';
-run_recon_mb_vbvd('/space/maki7/users/fhlin/smsini_array_nccu/063019/meas/32ch_TWIX/',sprintf('%s/%s/',pdir,subfolder_for_meas));
-
