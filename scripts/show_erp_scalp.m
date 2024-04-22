31a334a892d9a7dcf7679a384b63bbac478b3c13
Fa-Hsuan Lin <fahsuanlin@gmail.com>
2024-04-21 22:07:03 -0400

    Add files via upload

--- scripts/show_erp_scalp.m
+++ scripts/show_erp_scalp.m
@@ -1,29 +1,24 @@
 close all; clear all;
 
-load erp_avg_outside.mat
+load erp_avg_outside.mat;
+%read the 3rd condition; this is subject to change
+erp_condition_index=3;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 tmp=[];
-for i=1:size(erp,1)
+for i=1:size(erp_avg,1)
      if(isempty(tmp))
-        tmp=erp{i,3}.erp;
+        tmp=erp_avg{i,erp_condition_index}.erp;
     else
-        tmp=tmp+erp{i,3}.erp;
+        tmp=tmp+erp{i,erp_condition_index}.erp;
     end;
 end;
-ERP=tmp./size(erp,1);
-timeVec=erp{1,3}.timeVec;
+vep=tmp./size(erp_avg,1);
+timeVec=erp_avg{1,erp_condition_index}.timeVec;
 
-
-%load('/Users/fhlin_admin/workspace/eegmri/180322_PYW/analysis/bem.mat');
-%verts_osc_electrode_idx([32 33 34])=[];
-load('eeg_fwd_prep_042319.mat');
-for v_idx=1:length(points)
-    dist=verts_osc-repmat(points(v_idx,:),[size(verts_osc,1) 1]);
-    dist=sqrt(sum(dist.^2,2));
-    [dummy,verts_osc_electrode_idx(v_idx)]=min(dist);
-end;
-etc_render_topo('vol_vertex',verts_osc,'vol_face',faces_osc-1,'topo_vertex',verts_osc_electrode_idx-1,'topo_stc',ERP,'topo_smooth',20,'topo_threshold',[1 2],'topo_stc_timevec',timeVec,'topo_stc_timevec_unit','s');
-view(0,0);
+%31-channel EEG topography definition and a head model for Brain Products
+load topology_31ch_default.mat;
+etc_render_topo('vol_vertex',vertex,'vol_face',face-1,'topo_vertex',electrode_idx-1,'topo_stc',vep,'topo_smooth',10,'topo_threshold',[5 10],'topo_stc_timevec',timeVec,'topo_stc_timevec_unit','s');
 
 
 return;
