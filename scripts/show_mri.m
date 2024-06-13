1107d4924162e33cffe0edef78d3d6aac8670614
Fa-Hsuan Lin <fahsuanlin@gmail.com>
2024-06-12 21:18:02 -0400

    Add files via upload

--- scripts/show_mri.m
+++ scripts/show_mri.m
@@ -1,19 +1,19 @@
 close all; clear all;
 
-subject='s031';
+subject='s001';
 hemi='rh';
-surf='inflated';
+surf='white';
 
 
-file_overlay_register='../fmri_data/unpack/register.dat';
-file_overlay_vol='../fmri_data/unpack/bold/006/f.mgz';
-overlay_threshold=[100 1500];
+file_overlay_register='./bb_register_init.dat';
+file_overlay_vol='../data/unpack/gre/007/f.nii';
+overlay_threshold=[];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-setenv('SUBJECTS_DIR','/Users/fhlin/workspace/seeg/subjects/'); %for MAC/Linux
+setenv('SUBJECTS_DIR','/Users/fhlin/workspace/fieldmap_sri/subjects/'); %for MAC/Linux
 %setenv('SUBJECTS_DIR','D:\fhlin\Users\fhlin\workspace\seeg\subjects'); %for PC
 
-mri=MRIread(sprintf('/Users/fhlin/workspace/seeg/subjects/%s/mri/orig.mgz',subject)); %for MAC/Linux
+mri=MRIread(sprintf('/Users/fhlin/workspace/fieldmap_sri/subjects/%s/mri/orig.mgz',subject)); %for MAC/Linux
 %mri=etc_MRIread('D:\fhlin\Users\fhlin\workspace\seeg\subjects\2036\mri\orig.mgz'); %for PC
 
 %load the Talairach transformation matrix from the "pre-OP" data
@@ -30,4 +30,8 @@ vol = etc_MRIvol2vol(f,mri,r,'frames',[1]); %only the first volume
 %prepare overlay surface
 vol2surf = etc_MRIvol2surf(f,surf,r,'subject',subject,'frames',[1],'hemi',hemi);
 
-etc_render_fsbrain('surf',surf,'hemi',hemi,'subject',subject,'vol',mri,'talxfm',(talxfm),'overlay_vol',vol,'overlay_value',vol2surf,'overlay_vertex',[1:size(vol2surf,1)]-1,'overlay_threshold',overlay_threshold);
+etc_render_fsbrain('surf',surf,'hemi',hemi,'subject',subject,'vol',mri,'talxfm',(talxfm),'overlay_vol',vol,'overlay_value',vol2surf,'overlay_vertex',[1:size(vol2surf,1)]-1,'overlay_threshold',overlay_threshold,'overlay_flag_paint_on_cortex',0);
+
+%1. calcualte r * overlay_xfm after exporting the registration in Matlab work space
+%
+%2. check initial registration at command line: tkregisterfv --mov smsini_mb_run_1_ref.mgh --reg bb_register_init.dat --surfs  --sd /Users/fhlin/workspace/fieldmap_sri/subjects/
--- scripts/vol2surf.m
+++ scripts/vol2surf.m
@@ -1,26 +1,20 @@
 close all; clear all;
 
 
-file_register='../../register.dat';
 %provide the registration to structural freesurfer data
+file_register='./bb_register_init.dat'; %manual registration initialization file
 
-
+%field map (in rad)
 file_output={
-'sfmcprstc';
+'../data/unpack/fmap_rads.nii.gz';
 };
 
-TR=2.0; %second
+file_output_stem={
+'fmap_rads';
+};
 
-source_subject='190629_EP56';
+source_subject='s001';
 target_subject='fsaverage';
-dirs={
-'bold/004';
-'bold/006';
-'bold/011';
-'bold/013';
-'bold/017';
-'bold/019';
-};
 
 %provide the output file name for InI reconstruction
 
@@ -30,44 +24,39 @@ dirs={
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 pdir=pwd;
-for d_idx=1:length(dirs)
-	dd=sprintf('%s/%s',pdir,dirs{d_idx});
-	cd(dd);
-
 	for idx=1:length(file_output)
 	%do this outside matlab....
 	%make sure freesurfer environment, register file, and subjects directory are all set.
-		fn0=sprintf('%s.nii',file_output{idx});
-		fn1=sprintf('%s-lh.mgh',file_output{idx});
-		fn2=sprintf('%s-rh.mgh',file_output{idx});
+		fn0=sprintf('%s',file_output{idx});
+		fn1=sprintf('./%s-lh.mgh',file_output_stem{idx});
+		fn2=sprintf('./%s-rh.mgh',file_output_stem{idx});
 		eval(sprintf('!mri_vol2surf --icoorder 5 --fwhm 10 --src %s --srcreg %s --hemi lh --noreshape --out %s',fn0,file_register,fn1));
 		eval(sprintf('!mri_vol2surf --icoorder 5 --fwhm 10 --src %s --srcreg %s --hemi rh --noreshape --out %s',fn0,file_register,fn2));
 
 		brain_lh = MRIread(fn1);
-		fn3=sprintf('%s-lh.stc',file_output{idx});
+		fn3=sprintf('%s-lh.stc',file_output_stem{idx});
 		stc=squeeze(brain_lh.vol); if(min(size(stc))==1) stc=stc'; end;
-		inverse_write_stc(stc,[0:brain_lh.nvoxels-1],0,TR.*1e3,fn3);
+		inverse_write_stc(repmat(stc, [1 5]),[0:brain_lh.nvoxels-1],0,1e3,fn3); %fake TR = 1 s; repeat 5 times
 
 		brain_rh = MRIread(fn2);
-		fn4=sprintf('%s-rh.stc',file_output{idx});
+		fn4=sprintf('%s-rh.stc',file_output_stem{idx});
 		stc=squeeze(brain_rh.vol); if(min(size(stc))==1) stc=stc'; end;
-		inverse_write_stc(stc,[0:brain_rh.nvoxels-1],0,TR.*1e3,fn4);
+		inverse_write_stc(repmat(stc, [1 5]),[0:brain_rh.nvoxels-1],0,1e3,fn4); %fake TR = 1 s; repeat 5 times
 
 		%morphing
         	fn_in=fn3;
-        	fn_out=sprintf('%s_2_%s_%s',source_subject,target_subject,file_output{idx});
+        	fn_out=sprintf('%s_2_%s_%s',source_subject,target_subject,file_output_stem{idx});
         	cmd=sprintf('!mne_make_movie --subject %s --stcin %s --morph %s --stc %s --%s --smooth 5', source_subject, fn_in, target_subject, fn_out, 'lh');
         	eval(cmd);
 
         	fn_in=fn4;        
-		fn_out=sprintf('%s_2_%s_%s',source_subject,target_subject,file_output{idx});
+		fn_out=sprintf('%s_2_%s_%s',source_subject,target_subject,file_output_stem{idx});
         	cmd=sprintf('!mne_make_movie --subject %s --stcin %s --morph %s --stc %s --%s --smooth 5', source_subject, fn_in, target_subject, fn_out, 'rh');
         	eval(cmd);
 
-
 		eval(sprintf('!rm %s %s',fn1,fn2));
+		%eval(sprintf('!rm %s %s %s %s',fn1,fn2,fn3,fn4));
 	end;
-end;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-cd(pdir);
+
 fprintf('DONE!\n');
