bd414f99097bade6a9e430a2e352faee5901174d
Fa-Hsuan Lin <fahsuanlin@gmail.com>
2025-06-30 20:28:43 +0800

    Add files via upload

--- scripts/render_seeg_wb_dspm_cc_fus_target_e4_raw_vol_032322.m
+++ scripts/render_seeg_wb_dspm_cc_fus_target_e4_raw_vol_032322.m
@@ -17,8 +17,8 @@ flag_show_med=1;
 flag_show_lat=1;
 flag_show_electrode=1;
 
+surface_coord=[1.50 -35.56  29.40]; %this is the surface coordinate to determine sagittal/coronal/axial slices
 target_timeVec=[-50:25:500];
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %baseline=[-200 -20];
@@ -40,44 +40,32 @@ end;
 
 for stc_idx=1:length(file_stc)
     for trial_idx=1:length(trial)
-%         [stc_lh_x,v_lh,a,b,timeVec]=inverse_read_stc(sprintf('%s_trial%03d_x-lh.stc',file_stc{stc_idx},trial(trial_idx)));
-%         [stc_rh_x,v_rh,a,b,timeVec]=inverse_read_stc(sprintf('%s_trial%03d_x-rh.stc',file_stc{stc_idx},trial(trial_idx)));
-%         [stc_lh_y,v_lh,a,b,timeVec]=inverse_read_stc(sprintf('%s_trial%03d_y-lh.stc',file_stc{stc_idx},trial(trial_idx)));
-%         [stc_rh_y,v_rh,a,b,timeVec]=inverse_read_stc(sprintf('%s_trial%03d_y-rh.stc',file_stc{stc_idx},trial(trial_idx)));
-%         [stc_lh_z,v_lh,a,b,timeVec]=inverse_read_stc(sprintf('%s_trial%03d_z-lh.stc',file_stc{stc_idx},trial(trial_idx)));
-%         [stc_rh_z,v_rh,a,b,timeVec]=inverse_read_stc(sprintf('%s_trial%03d_z-rh.stc',file_stc{stc_idx},trial(trial_idx)));
-%         stc_lh=sqrt(stc_lh_x.^2+stc_lh_y.^2+stc_lh_z.^2);
-%         stc_rh=sqrt(stc_rh_x.^2+stc_rh_y.^2+stc_rh_z.^2);
          [stc_lh,v_lh,a,b,timeVec]=inverse_read_stc(sprintf('%s-lh.stc',file_stc{stc_idx}));
          [stc_rh,v_rh,a,b,timeVec]=inverse_read_stc(sprintf('%s-rh.stc',file_stc{stc_idx}));
         
 
-%         t_idx=find((timeVec>=min(timeVec_range))&(timeVec<=max(timeVec_range)));
-%         timeVec=timeVec(t_idx);
-%         stc_lh=stc_lh(:,t_idx);
-%         stc_rh=stc_rh(:,t_idx);
-
         im2_med=[];
         im2_lat=[];
         
-%         timeVec=timeVec(1:20:end);
-%         stc_lh=stc_lh(:,1:20:end);
-%         stc_rh=stc_rh(:,1:20:end);
         
-        output_fstem=sprintf('%s_trial%03d',file_stc{stc_idx},trial(trial_idx));
+        output_fstem=sprintf('%s_trial%03d_vol',file_stc{stc_idx},trial(trial_idx));
         
         for hemi_idx=1:length(hemi)
             
             clear global etc_render_fsbrain;
             ff_med=figure; set(ff_med,'visible','off');
             if(strcmp(hemi{hemi_idx},'lh'))
-                etc_render_fsbrain('subject',subject,'hemi','lh','overlay_stc',stc_lh,'overlay_vertex',v_lh,'overlay_threshold',threshold{stc_idx},'vol_A',A,'overlay_smooth',5,'view_angle',[90,0]);
+                etc_render_fsbrain('subject',subject,'hemi','lh','overlay_stc',stc_lh,'overlay_vertex',v_lh,'vol_A',A,'overlay_threshold',threshold{stc_idx},'overlay_smooth',5,'view_angle',[90,0]);
                 view(90,0);
                 view(-90,0);
+                etc_render_fsbrain_handle('draw_pointer','surface_coord',surface_coord);
+
             else
-                etc_render_fsbrain('subject',subject,'hemi','rh','overlay_stc',stc_rh,'overlay_vertex',v_rh,'overlay_threshold',threshold{stc_idx},'vol_A',A,'overlay_smooth',5,'view_angle',[-90,0]);
+                etc_render_fsbrain('subject',subject,'hemi','rh','overlay_stc',stc_rh,'overlay_vertex',v_rh,'vol_A',A,'overlay_threshold',threshold{stc_idx},'overlay_smooth',5,'view_angle',[-90,0]);
                 view(-90,0);
                 view(90,0);
+                etc_render_fsbrain_handle('draw_pointer','surface_coord',surface_coord);
+                
             end;            
             
             if(flag_show_electrode)
@@ -122,35 +110,16 @@ for stc_idx=1:length(file_stc)
                 set(ff_med,'visible','off');
             end;
             
+
             [~, timeVec_idx] = min( abs(timeVec(:) - target_timeVec(:).'), [], 1 );
             for time_idx=1:length(timeVec_idx)
                 fprintf('rendering <%s>:: [%0d|%04d] (timeVec=%1.1f; %2.2f%%)...\r',output_fstem, time_idx,length(timeVec_idx),timeVec(timeVec_idx(time_idx)),time_idx./length(timeVec_idx).*100);
-                
-                if(flag_show_med)
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    % medial view
-                    etc_render_fsbrain_update_time(timeVec_idx(time_idx));
-                    if(strcmp(hemi{hemi_idx},'lh'))
-                        view(90,0);
-                        hgexport(gcf,sprintf('img/%s_med_lh_f%04d',output_fstem,time_idx),hgexport('factorystyle'),'Format','png');
-                    else
-                        view(-90,0);                        
-                        hgexport(gcf,sprintf('img/%s_med_rh_f%04d',output_fstem,time_idx),hgexport('factorystyle'),'Format','png');
-                    end;
-                end;
-                
-                if(flag_show_lat)
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    % lateral view
-                    etc_render_fsbrain_update_time(timeVec_idx(time_idx));
-                    if(strcmp(hemi{hemi_idx},'lh'))
-                        view(-90,0);
-                        hgexport(gcf,sprintf('img/%s_lat_lh_f%04d',output_fstem,time_idx),hgexport('factorystyle'),'Format','png');
-                    else
-                        view(90,0);                        
-                        hgexport(gcf,sprintf('img/%s_lat_rh_f%04d',output_fstem,time_idx),hgexport('factorystyle'),'Format','png');
-                    end;
-                end;
+
+                etc_render_fsbrain_update_time(timeVec_idx(time_idx));
+                etc_render_fsbrain_handle('redraw');
+                etc_render_fsbrain_handle('draw_pointer','surface_coord',surface_coord);
+                figure(etc_render_fsbrain.fig_vol);
+                hgexport(gcf,sprintf('img/%s_f%04d',output_fstem,time_idx),hgexport('factorystyle'),'Format','png');
             end;
         end;        
 
@@ -165,36 +134,16 @@ for stc_idx=1:length(file_stc)
         for time_idx=1:length(timeVec_idx)
             for hemi_idx=1:length(hemi)
                 fprintf('create animation frame <%s>::<<%s>> [%0d|%04d] (timeVec=%1.1f; %2.2f%%)...\r',output_fstem, hemi{hemi_idx},time_idx,length(timeVec_idx),timeVec(timeVec_idx(time_idx)),time_idx./length(timeVec_idx).*100);
-                im2_lh_lat=[];
-                im2_rh_lat=[];
-                im2_lh_med=[];
-                im2_rh_med=[];
                 
-                if(strcmp(hemi{hemi_idx},'lh'))
-                    if(flag_show_lat)
-                        im2_lh_lat=imread(sprintf('img/%s_lat_lh_f%04d.png',output_fstem,time_idx));
-                    end;
-                    if(flag_show_med)
-                        im2_lh_med=imread(sprintf('img/%s_med_lh_f%04d.png',output_fstem,time_idx));
-                    end;
-                end;
-                if(strcmp(hemi{hemi_idx},'rh'))
-                    if(flag_show_lat)
-                        im2_rh_lat=imread(sprintf('img/%s_lat_rh_f%04d.png',output_fstem,time_idx));
-                    end;
-                    if(flag_show_med)
-                        im2_rh_med=imread(sprintf('img/%s_med_rh_f%04d.png',output_fstem,time_idx));
-                    end;
-                end;
+                tmp=imread(sprintf('img/%s_f%04d.png',output_fstem,time_idx));
             end;
             
-            ff_img=figure; set(ff_img,'visible','off');
-            tmp=cat(2,im2_lh_lat,im2_lh_med,im2_rh_med,im2_rh_lat);
+            ff_img=figure;
             image(tmp);
             axis off image;
             set(gca,'pos',[0 0 1 1]);
             set(gcf,'pos',[200         800        size(tmp,2)*2         size(tmp,1)*2]);
-            h=text(40, 340,sprintf('%0.0f ms',timeVec(timeVec_idx(time_idx))));
+            h=text(650, 460, sprintf('%0.0f ms',timeVec(timeVec_idx(time_idx))));
             set(h,'fontname','helvetica','fontsize',40,'color','b');
             hgexport(ff_img,sprintf('img/%s_tmp',output_fstem),hgexport('factorystyle'),'Format','png');
             im=imread(sprintf('img/%s_tmp.png',output_fstem));
